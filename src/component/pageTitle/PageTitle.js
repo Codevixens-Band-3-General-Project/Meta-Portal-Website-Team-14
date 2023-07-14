aa47cc839c6a0be9f1e7db82fd0d1e694bc8ad56
Gelaraph <angelaraph12@gmail.com>
2023-07-14 13:37:31 +0100

    fixes

---
+++ src/component/pageTitle/PageTitle.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import "./pageTitle.css";
+
+const PageTitle = ({ title }) => (
+  <div className="metaportal_fn_pagetitle">
+    <div className="container small">
+      <div className="pagetitle">
+        <h3 className="fn__maintitle big">{title}</h3>
+        <p>
+          <Link to="/">Home</Link>
+          <span className="divider">/</span>
+          <span className="text">{title}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
+export default PageTitle;
