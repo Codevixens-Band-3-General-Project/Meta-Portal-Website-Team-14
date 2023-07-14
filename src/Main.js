91d1ac86ae8e6f8428d58d801475ad52fb2be20a
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-07-14 15:24:55 +0200

    Roadmap

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 
 import Contact from "./component/ContactSection/Contact";
+import RoadMap from "./component/Roadmap/Roadmap";
 import SectionDivider from "./component/SectionDivider/SectionDivider";
 
 const Main = ({ carousel }) => (
   <>
     {carousel}
+    <SectionDivider />
+    <RoadMap />
     <SectionDivider />
     <Contact />
   </>
