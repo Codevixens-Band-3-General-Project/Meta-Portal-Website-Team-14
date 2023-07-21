86b3c8db3db65116ac7347cd9f21fd2f93785531
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-07-21 11:49:09 +0300

    Revert "Jackline"

--- src/component/PageTitle/PageTitle.js
+++ src/component/PageTitle/PageTitle.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import "./PageTitle.css";
+import "./pageTitle.css";
 
 const PageTitle = ({ title }) => (
   <div className="metaportal_fn_pagetitle">
