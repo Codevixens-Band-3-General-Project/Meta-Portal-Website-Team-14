f7ab7b0c7ff31cef1ccbeb43564afac940e8d86e
Gelaraph <angelaraph12@gmail.com>
2023-07-21 10:23:49 +0100

    merge conflict

--- src/Main.js
+++ src/Main.js
@@ -23,7 +23,7 @@ import Privacy from "./component/privacy/Privacy";
 import About from "./component/About";
 import Contact from "./component/contactSection/Contact";
 import Blog from "./component/Blog";
-import RoadMap from "./component/roadmap/Roadmap";
+import RoadMap from "./component/roadMap/Roadmap";
 import MintPage from "./component/mint/MintPage";
 
 const Main = () => {
